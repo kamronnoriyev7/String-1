cc05e51f749f416af3666be2f4d1e2af477d1d8c
kamronnoriyev <kamronnoriyev7@gmail.com>
2024-11-09 22:31:03 +0500

    .

--- Program.cs
+++ Program.cs
@@ -13,16 +13,130 @@ namespace string1
             // Console.Write("so'zni kirit: ");
             // var string1 = Console.ReadLine();
             // Console.WriteLine($"Hello {string1}");
-            
+
             // 2
             // Console.Write("1 - so'zni kirit: ");
             // var string1 = Console.ReadLine();
             // Console.Write("2 - so'zni kirit: ");
             // var string2 = Console.ReadLine();
             // Console.WriteLine(string1+string2+string2+string1);
-            
+
             // 3
+            // Console.Write("satr1: ");
+            // var firsttext = Console.ReadLine();
+            // Console.Write("satr2: ");
+            // var secondtext = Console.ReadLine();
+            // var result = firsttext.Insert(2,secondtext);
+            // Console.WriteLine(result);
+
+            // 4
+            // Console.Write("satr: ");
+            // var satr = Console.ReadLine();
+            // var result = satr.Substring(satr.Length - 2);
+            // Console.WriteLine(result+result+result);
+
+            // 5
+            // Console.Write("satr: ");
+            // var satr = Console.ReadLine();
+            // if (satr.Length<2)
+            // {
+            //     Console.WriteLine(satr);
+            // }
+            // else
+            // {
+            //     var result = satr.Substring(0,2);
+            //     Console.WriteLine(result);
+            // }
+
+            // 6
+            // Console.Write("satr: ");
+            // var text = Console.ReadLine();
+            // var texthalf = text.Substring(0, text.Length / 2);
+            // Console.WriteLine(texthalf);
+
+            // 7
+            // Console.WriteLine("satr: ");
+            // var text = Console.ReadLine();
+            // var words = text.Substring(1, text.Length - 2);
+            // Console.WriteLine(words);
+
+            // 8
+            // Console.WriteLine("1 satr: ");
+            // var text1 = Console.ReadLine();
+            // Console.WriteLine("2 satr: ");
+            // var text2 = Console.ReadLine();
+            // var textedit1 = text1.Length;
+            // var textedit2 = text2.Length;
+            // if (textedit1<textedit2)
+            // {
+            //     Console.WriteLine(text1+text2+text1);
+            // }
+            // else
+            // {
+            //     Console.WriteLine(text2+text1+text2);
+            // }
+
+            // 9
+            // Console.Write("satr1: ");
+            // var text1 = Console.ReadLine();
+            // Console.Write("satr2: ");
+            // var text2 = Console.ReadLine();
+            // var edittext1 = text1.Remove(0,1);
+            // var edittext2 = text2.Remove(0,1);
+            // Console.WriteLine(edittext1 + edittext2);
+
+
+            // 10
+            // Console.WriteLine("satr: ");
+            // var text = Console.ReadLine();
+            // var textEdit = text.Substring(0, 2);
+            // var textInsert = text.Substring(2);
+            // Console.WriteLine(textInsert+textEdit);
+
+            // 11
+            // Console.Write("satr: ");
+            // var text = Console.ReadLine();
+            // Console.Write("bool: ");
+            // var boolText = bool.Parse(Console.ReadLine());
+            // if (boolText == true)
+            // {
+            //     Console.WriteLine(text.Substring(0,1));
+            // }
+            // else
+            // {
+            //     Console.WriteLine(text.Substring(text.Length-1));
+            // }
+            //     
+
+            // 12
+            // Console.Write("satr: ");
+            // var text = Console.ReadLine();
+            // Console.Write("son: ");
+            // var index = int.Parse(Console.ReadLine());
+            // var firsttext = text.Substring(0, index);
+            // var lasttext = text.Substring(text.Length - index);
+            // Console.WriteLine(firsttext+lasttext);
+
+            // 13
+            // Console.Write("satr: ");
+            // var text = Console.ReadLine();
+            // var textedit = text.Substring(text.Length / 2 - 1, 3);
+            // Console.WriteLine(textedit);
             
+            // 14
+            // Console.Write("satr: ");
+            // var text1 = Console.ReadLine();
+            // Console.Write("satr: ");
+            // var text2 = Console.ReadLine();
+            // if (text1[text1.Length-1]==text2[0])
+            // {
+            //     var text3 = text2.Remove(0,1);
+            //     Console.Write(text1+text3);
+            // }
+            // else
+            // {
+            //     Console.Write(text1+text2);
+            // }
             
             
             
