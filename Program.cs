815a2ffe93d52999a343bf364cae1ba63986bb93
kamronnoriyev <kamronnoriyev7@gmail.com>
2024-11-09 21:04:50 +0500

    .

--- Program.cs
+++ Program.cs
@@ -9,7 +9,29 @@ namespace string1
     {
         static void Main(string[] args)
         {
-           
+            // 1
+            // Console.Write("so'zni kirit: ");
+            // var string1 = Console.ReadLine();
+            // Console.WriteLine($"Hello {string1}");
+            
+            // 2
+            // Console.Write("1 - so'zni kirit: ");
+            // var string1 = Console.ReadLine();
+            // Console.Write("2 - so'zni kirit: ");
+            // var string2 = Console.ReadLine();
+            // Console.WriteLine(string1+string2+string2+string1);
+            
+            // 3
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
             
             
             
